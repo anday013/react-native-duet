629bc2162eddd38cafffc964f15668e8a58e4805
Anday <48630069+anday2000@users.noreply.github.com>
2021-04-13 05:41:51 +0400

    :tada:

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ufaz Project</Text>
+      <Text>Thesis Project</Text>
     </View>
   );
 }
