5482b94598dfa3884bb130cc65ed1b9dd0f506e3
Anday <48630069+anday2000@users.noreply.github.com>
2021-04-13 08:03:06 +0400

    :heavy_plus_sign: React Native Image Picker

--- src/screens/VideoEncoder.js
+++ src/screens/VideoEncoder.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import Button from '../components/Button';
+import {perH} from '../constants';
+const OPTIONS = {mediaType: 'video'};
 export default function VideoEncoder() {
-  return <SafeAreaView style={styles.container}></SafeAreaView>;
+  const [firstVideoUri, setFirstVideoUri] = useState('');
+  const [secondVideoUri, setSecondVideoUri] = useState('');
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.buttonsWrapper}>
+        <Button
+          title="Choose First Video"
+          style={styles.button1}
+          onPress={() =>
+            launchImageLibrary(OPTIONS, res => setFirstVideoUri(res.uri))
+          }
+        />
+        <Button
+          title="Choose Second Video"
+          style={styles.button2}
+          onPress={() =>
+            launchImageLibrary(OPTIONS, res => setSecondVideoUri(res.uri))
+          }
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginVertical: perH(2),
+  },
+  buttonsWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  button1: {
+    backgroundColor: '#d90429',
+  },
+  button2: {
+    backgroundColor: '#fb8500',
   },
 });
